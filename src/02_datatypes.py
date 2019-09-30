7e7758b6d3688fb45c2c6ad08ec20c90c8580c60
abidex4yemi <abidex4yemi.com>
2019-09-30 16:07:50 +0100

    complete datatypes exercise

--- src/02_datatypes.py
+++ src/02_datatypes.py
@@ -14,8 +14,15 @@ y = "7"
 # Write a print statement that combines x + y into the integer value 12
 
 # YOUR CODE HERE
-
+# Note: explicit type conversion (Casting) of (y) to integer
+sum_of_two_numbers = x + int(y)
+print(sum_of_two_numbers)
+print(type(sum_of_two_numbers))
 
 # Write a print statement that combines x + y into the string value 57
 
 # YOUR CODE HERE
+# Note: explicit type conversion (Casting) of (x) to string
+string_version = str(x) + y
+print(string_version)
+print(type(string_version))
